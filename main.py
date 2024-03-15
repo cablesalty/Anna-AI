ae8050b7cc43f776c9f83ae10c385ba47cc13a9c
cablesalty <cablesalty@gmail.com>
2024-03-16 00:23:29 +0100

    Licensz frissítése

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # Anna-AI (https://github.com/cablesalty/Anna-AI) Main File. 
+
+# ================================================================================
+# GNU GENERAL PUBLIC LICENSE
+# Version 3, 29 June 2007
+#
+# Copyright (C) 2024 Szabó Patrik
 #
 # Anna-AI is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -12,6 +18,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ================================================================================
 
 __author__ = "cablesalty"
 
